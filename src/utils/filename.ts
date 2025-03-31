801f27101d518c0d4986d82d0d05d139ff6df581
Riceball LEE <snowyu.lee@gmail.com>
2025-03-31 08:19:43 +0800

    refactor: extract getMultiLevelExtname to @isdk/util

--- src/utils/filename.ts
+++ src/utils/filename.ts
@@ -1,6 +1,8 @@
 import path from 'path'
 import { ErrorCode, throwError } from './base-error';
 
+export { getMultiLevelExtname } from '@isdk/util';
+
 /**
  * Regular expression pattern for reserved characters in a filename.
  * do not use /g global option here: when the regex is executed multiple times, it will always begin where it left off last time.
@@ -163,26 +165,6 @@ export function sanitizeFilepath(filepath:string, options: SanitizeFilenameOptio
 
 	// Rejoin the sanitized parts using the appropriate path separator
 	return result.join(path.sep)
-}
-
-/**
- * Retrieves multi-level file extension
- * @param filename The file name
- * @param level The level, default is 1, indicating the number of extension levels to retrieve
- * @returns Returns a concatenated string of multiple extensions, or an empty string if no extension is found
- */
-export function getMultiLevelExtname(filename: string, level: number = 1) {
-  let result = '' // Initialize the result string for storing extensions
-  while (level--) { // Decrement level and loop until the specified level is reached
-    const extname = path.extname(filename) // Get the current file's extension
-    if (extname) {
-      result = extname + result // Prepend the current extension to the result string
-      filename = path.basename(filename, extname) // Update filename by removing the obtained extension
-    } else {
-      break // If no extension exists, exit the loop
-    }
-  }
-  return result // Return the concatenated extension string
 }
 
 /**
