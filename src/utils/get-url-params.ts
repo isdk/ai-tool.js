83b8cc5a2b479e8bd0c6793a63dcaaaf54814f58
Riceball LEE <snowyu.lee@gmail.com>
2025-08-31 08:50:32 +0800

    feat: add getUrlParams func

---
+++ src/utils/get-url-params.ts
@@ -0,0 +1,15 @@
+  /**
+   * Utility to serialize an object into a URL query parameter string.
+   * @param {any} objParam - The parameter object.
+   * @returns {string} The URL-encoded query string or an empty string.
+   */
+export function getUrlParams(objParam: any) {
+  if (objParam !== undefined) {
+    const objParamStr = JSON.stringify(objParam)
+    if (objParamStr !== '{}' && objParamStr !== '[]' && objParamStr !== '""') {
+      // not empty params
+      return '?p=' + encodeURIComponent(objParamStr)
+    }
+  }
+  return ''
+}
