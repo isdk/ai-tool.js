4d774ad8f0631d602ff0f12543881d90ce16e84b
Riceball LEE <snowyu.lee@gmail.com>
2025-09-10 18:45:10 +0800

    fix: encode uuid not work feat: add v6, v7 uuid feat: add uuid(encode?: boolean) overloading docs: add tsdoc test: test uuid(encode) added

--- src/utils/hash/uuid.test.ts
+++ src/utils/hash/uuid.test.ts
@@ -1,4 +1,6 @@
-import { uuid, uuidValidate } from './uuid'
+import * as base32768 from 'base32768';
+
+import { uuid, uuidValidate, uuidStringify } from './uuid'
 
 describe("uuid", () => {
   it("should gen uuid", () => {
@@ -6,5 +8,13 @@ describe("uuid", () => {
     expect(result).toBeDefined()
     expect(result).toHaveLength(36)
     expect(uuidValidate(result)).toBe(true)
+  })
+
+  it("should gen uuid with encoding", () => {
+    let result = uuid(true)
+    expect(result).toBeDefined()
+    const uuidStr = uuidStringify(base32768.decode(result))
+    expect(uuidStr).toHaveLength(36)
+    expect(uuidValidate(uuidStr)).toBe(true)
   })
 })
