9d99213fe19bc5332c529fd30fab63a018d77079
Riceball LEE <snowyu.lee@gmail.com>
2025-11-15 22:05:23 +0800

    fix(ts): should re-export hash to keep compatibility

--- src/utils/hash/index.ts
+++ src/utils/hash/index.ts
@@ -1,2 +1,2 @@
-export * from '@isdk/hash'
+export {type HashAlgoParams, HashAlgorithm, type UUIDVersions, base32768, hash, hashObject, hashStream, uuid, xxhash, xxhash32, xxhash64, xxhashAsStr} from '@isdk/hash'
 
